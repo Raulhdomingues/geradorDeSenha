60ed9fe3dbc85055b8013c110c5702bfc62d172d
Raul <raulhdomingues@hotmail.com>
2023-05-26 21:30:42 -0300

    update

--- js/script.js
+++ js/script.js
@@ -3,8 +3,10 @@ const generatePasswordButton = document.querySelector("#generate-password");
 const generatePasswordElement = document.querySelector("#generated-password");
 
 //Funções
-
+const getLetterLowerCase = () => {
+    console.log(String.fromCharCode(Math.floor(Math.random() * 26)));
+};
 //Eventos
 generatePasswordButton.addEventListener("click", () =>{
     console.log("teste");
-})
+});
